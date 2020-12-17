5a8173ce9b2e9655f5d3db4a745a222fd916667b
Fabian Byrd <fabianbyrd@Fabosmac.attlocal.net>
2020-12-17 15:39:56 -0500

    update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budget-tracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
