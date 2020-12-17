efa40034907a5d8f624968d3fc5507fc182183c7
Fabian Byrd <fabianbyrd@Fabosmac.attlocal.net>
2020-12-17 15:55:48 -0500

    updates to database name

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 var db = require('../models');
 
 //make the connection to the db using mongoose
-mongoose.connect(process.env.MONGODB.URI || 'mongodb://localhost/budget', {
+mongoose.connect(process.env.MONGODB.URI || 'mongodb://localhost/budget-tracker', {
     useNewUrlParser: true
 });
